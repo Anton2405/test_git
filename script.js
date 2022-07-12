8496693eda360d2d1a2e2183034ccadf013da131
Anton2405 <antonvershkov@mail.ru>
2022-07-12 11:12:55 +0300

    222

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //console.log("привет");
-const person = Object.create({},{
+//console.log("привет");
+const person = Object.create({}, {
     name: {
         value: 'Anton'
     },
     birthYear
 })
+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
